297f12d9629dc7bc2cfee899489441f0946c1d99
RonalYesidGutierrezQuitian <152307741+RonalYesidGutierrezQuitian@users.noreply.github.com>
2024-01-09 06:37:26 -0500

    Delete 8.torneoPingPong directory
    Deleted 8.torneoPingPong directory

--- 8.torneoPingPong/categorias.py
+++
@@ -1,9 +0,0 @@
-def defCategoria(categoriaJ : int) -> str:
-    categoria = ""
-    if (categoriaJ == 1):
-        categoria = "Novato"
-    elif (categoriaJ == 2):
-        categoria = "Intermedio"
-    elif (categoriaJ == 3):
-        categoria = "Avanzado"
-    return categoria
